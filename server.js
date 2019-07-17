5b8c626d4b777375e93b22302e3e50bf447d6a86
PWFollett <pwf592110@gmail.com>
2019-07-17 19:38:57 -0400

    trying again

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -48,6 +48,10 @@ app.get("/", function(req, res) {
 
 app.get("/add", function(req, res) {
   res.sendFile(path.join(__dirname, "add.html"));
+});
+
+app.get("/", function(req, res) {
+  res.json(path.join(__dirname, "public/index.html"));
 });
 
 // Displays all characters
